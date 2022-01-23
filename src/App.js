62b9061b38d69179ed3b4f13ca55d53886cbc3a6
SudhirSahoo <sudhirid@gmail.com>
2022-01-22 23:13:17 -0600

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am from feature branch!
         </a>
       </header>
     </div>
